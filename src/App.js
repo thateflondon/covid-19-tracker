6c2e61b2107d3d7aebde2fa5d912538125f666e9
Jerry Frederic <jerry.fpl@gmail.com>
2021-04-01 16:46:07 +0200

    Table data

--- src/App.js
+++ src/App.js
@@ -9,12 +9,27 @@ import {
 } from "@material-ui/core";
 import InfoBox from "./components/InfoBox";
 import Map from "./components/Map";
+import Table from "./components/Table";
 import "./style/App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("Worldwide");
+  //Individual country
+  const [countryInfo, setCountryInfo] = useState({});
+  //Table
+  const [tableData, setTableData] = useState([]);
 
+  //All the cases
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  //Cases in the selected country
   useEffect(() => {
     //Async API request
     const getCountriesData = async () => {
@@ -25,6 +40,8 @@ function App() {
             name: country.country, //France, Cameroon, United States
             value: country.countryInfo.iso2, //FR, CM, US
           }));
+
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -37,7 +54,22 @@ function App() {
     const countryCode = event.target.value;
     console.log("country code >>>", countryCode);
     setCountry(countryCode);
+
+    const url =
+      countryCode === "Worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        //All of the data from the country response
+        setCountryInfo(data);
+      });
   };
+
+  console.log("Country Info >>>", countryInfo);
 
   return (
     <div className="app">
@@ -63,11 +95,23 @@ function App() {
 
         <div className="app__stats">
           {/**InfoBox title="Coronavirus cases" */}
-          <InfoBox title="Coronavirus Cases" cases={122003} total={550005} />
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
           {/**InfoBox title="Coronavirus recoveries" */}
-          <InfoBox title="Recovered" cases={16223} total={55665} />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
           {/**InfoBox title="Deaths"*/}
-          <InfoBox title="Deaths" cases={51223} total={5505} />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
 
         {/**Map */}
@@ -78,7 +122,8 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           {/**Table */}
-          <h3>Worldwide new Cases</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide New Cases</h3>
           {/**Graph */}
         </CardContent>
       </Card>
---
+++ src/components/Table.js
@@ -0,0 +1,8 @@
+import React from "react";
+//import "./style/Table.css";
+
+function Table() {
+  return <div></div>;
+}
+
+export default Table;
