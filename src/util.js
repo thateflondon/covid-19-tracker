ce734e36620e89cea80965471a68994d62b5c986
Jerry Frederic <jerry.fpl@gmail.com>
2021-04-06 05:59:28 +0200

    Map build - Pop Up

--- src/util.js
+++ src/util.js
@@ -35,7 +35,7 @@ const casesTypeColors = {
 export const sortData = (data) => {
   const sortedData = [...data];
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
-  return sortedData;
+  //return sortedData;
   /*sortedData.sort((a, b) => {
     if (a.cases > b.cases) {
       return -1;
@@ -59,7 +59,22 @@ export const showDataOnMap = (data, casesType = "cases") =>
       }
     >
       <Popup>
-        <h1>Pop Up</h1>
+        <div className="info-container">
+          <div className="info-name">{country.country}</div>
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-cases">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
       </Popup>
     </Circle>
   ));
